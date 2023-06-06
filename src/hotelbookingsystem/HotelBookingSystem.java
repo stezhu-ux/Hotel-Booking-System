d4006166e20c312040485880af6fe5a64708aba5
stezhu-ux <117567066+stezhu-ux@users.noreply.github.com>
2023-06-07 04:37:02 +1200

    Main Menu GUI

--- src/hotelbookingsystem/HotelBookingSystem.java
+++ src/hotelbookingsystem/HotelBookingSystem.java
@@ -4,19 +4,142 @@ import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.io.File;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class HotelBookingSystem {
+public class HotelBookingSystem extends JFrame implements ActionListener {
 
     static ArrayList<HotelRoom> rooms = new ArrayList<>();
     public static ArrayList<Booking> bookings = new ArrayList<>();
     
-    private static Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in); 
+
+    private JButton bookButton;
+    private JButton checkoutButton;
+    private JButton viewAllButton;
+    private JButton viewUnoccupiedButton;
+    private JButton viewOccupiedButton;
+    private JButton viewSpecificButton;
+    private JButton findGuestButton;
+    private JButton listGuestsButton;
+    private JButton exitButton;
+
+    public HotelBookingSystem() {
+        setTitle("Hotel Booking System");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new GridLayout(9, 1));
+
+        bookButton = new JButton("Book a Room");
+        bookButton.addActionListener(this);
+        mainPanel.add(bookButton);
+
+        checkoutButton = new JButton("Check-out a Room");
+        checkoutButton.addActionListener(this);
+        mainPanel.add(checkoutButton);
+
+        viewAllButton = new JButton("View All Rooms and Details");
+        viewAllButton.addActionListener(this);
+        mainPanel.add(viewAllButton);
+
+        viewUnoccupiedButton = new JButton("View All Unoccupied Rooms");
+        viewUnoccupiedButton.addActionListener(this);
+        mainPanel.add(viewUnoccupiedButton);
+
+        viewOccupiedButton = new JButton("View All Occupied Rooms");
+        viewOccupiedButton.addActionListener(this);
+        mainPanel.add(viewOccupiedButton);
+
+        viewSpecificButton = new JButton("View Specific Room");
+        viewSpecificButton.addActionListener(this);
+        mainPanel.add(viewSpecificButton);
+
+        findGuestButton = new JButton("Find Guest by Name");
+        findGuestButton.addActionListener(this);
+        mainPanel.add(findGuestButton);
+
+        listGuestsButton = new JButton("List All Guests");
+        listGuestsButton.addActionListener(this);
+        mainPanel.add(listGuestsButton);
+
+        exitButton = new JButton("Exit");
+        exitButton.addActionListener(this);
+        mainPanel.add(exitButton);
+
+        add(mainPanel);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == bookButton) 
+        {
+            if (BookRoom.checkAvailability()) 
+            {
+                BookRoom bookRoom = new BookRoom();
+                JOptionPane.showMessageDialog(this, "Room booked successfully.");
+            } 
+            else 
+            {
+                JOptionPane.showMessageDialog(this, "Sorry, there are no unoccupied rooms available.");
+            }
+        }
+        
+        else if (e.getSource() == checkoutButton) 
+        {
+            if (CheckOut.checkAvailability()) 
+            {
+                CheckOut.checkOutGuest();
+                JOptionPane.showMessageDialog(this, "Room checked out successfully.");
+            } 
+            else 
+            {
+                JOptionPane.showMessageDialog(this, "Sorry, all rooms are unoccupied.");
+            }
+        }
+        
+        else if (e.getSource() == viewAllButton) 
+        {
+            RoomInfo.viewAllRooms();
+        }
+        
+        else if (e.getSource() == viewUnoccupiedButton) 
+        {
+            RoomInfo.listUnoccupiedRooms();
+        }
+        
+        else if (e.getSource() == viewOccupiedButton) 
+        {
+            RoomInfo.listOccupiedRooms();
+        }
+        
+        else if (e.getSource() == viewSpecificButton) 
+        {
+            RoomInfo.viewSpecificRoomNumber();
+        } 
+        
+        else if (e.getSource() == findGuestButton) 
+        {
+            RoomInfo.findGuestByName();
+        } 
+        
+        else if (e.getSource() == listGuestsButton) 
+        {
+            RoomInfo.listAllGuests();
+        }
+        
+        else if (e.getSource() == exitButton) 
+        {
+            System.out.println("Exiting program...");
+            System.exit(0);
+        }
+    }
     
     public static void main(String[] args) {
-        
-       //test
-       
-    //initialize hotel rooms
+        //initialize hotel rooms
+
     rooms.add(new StandardRoom(1, 100, BedType.SINGLE));
     rooms.add(new StandardRoom(2, 100, BedType.SINGLE));
     rooms.add(new DeluxeRoom(3, 300, 2, BedType.QUEEN));
@@ -30,92 +153,7 @@ public class HotelBookingSystem {
    } else {
        System.out.println("No bookings file found.");
    }
-    
-    System.out.println("-------------------------");
-    System.out.println("Welcome to Hotel Booking System app");
-    System.out.println("The purpose of this app is for hotel receptionists to book and check-out rooms for guests wanting to stay at their hotel.");
-    System.out.println("-------------------------");
-    
-    // main loop to handle user input
-    while (true) {
-        System.out.println("Please select an option:");
-        System.out.println("-------------------------");
-        System.out.println("Press 1 to: Book a room");
-        System.out.println("Press 2 to: Check-out a room");
-        System.out.println("Press 3 to: View all rooms and details");
-        System.out.println("Press 4 to: View all unoccupied rooms");
-        System.out.println("Press 5 to: View all occupied rooms");
-        System.out.println("Press 6 to: View specific room number");
-        System.out.println("Press 7 to: Find guest by name");
-        System.out.println("Press 8 to: List all current guest");
-        System.out.println("Press 9 to: Exit app");
-        
-        // try catch to handle user errors outside of number range        
-        try {
-            int option = scanner.nextInt();
-            scanner.nextLine(); // consume the newline character
-            
-            switch (option) {
-                 
-                    case 1:
-                        //  check if any unoccupied rooms available
-                    if (BookRoom.checkAvailability()) {
-                        BookRoom bookRoom = new BookRoom();
-                    } else {
-                        System.out.println("Sorry, there are no unoccupied rooms available");
-                        System.out.println("-------------------------");
-                    }
-    
-                break;
-                    
-                case 2:
-                    if (CheckOut.checkAvailability()) {
-                    CheckOut.checkOutGuest();
-                    } else {
-                     System.out.println("Sorry, all rooms are unoccupied");
-                     System.out.println("-------------------------");
-                    }
-                    
-                    break;
-                    
-                case 3:    
-                    RoomInfo.viewAllRooms();
-                    break;
-                    
-                case 4:
-                    RoomInfo.listUnoccupiedRooms();
-
-                    break;
-                    
-                case 5:
-                     RoomInfo.listOccupiedRooms();
-                    
-                    break;
-                    
-                case 6: 
-                    RoomInfo.viewSpecificRoomNumber();
-                    break;
-                    
-                case 7:
-                    RoomInfo.findGuestByName();
-                    break;
-                    
-                case 8:
-                     RoomInfo.listAllGuests();
-                    break;
-                
-                case 9:
-                    System.out.println("Exiting program...");
-                    System.exit(0);
-                    
-                default:
-                    System.out.println("Invalid option, please try again");
-            }
-        } catch (InputMismatchException e) {
-            System.out.println("Invalid input. Please try again");
-            scanner.nextLine(); //consumes invalid input
-        }
+        HotelBookingSystem GUI = new HotelBookingSystem();
+        GUI.setVisible(true);
     }
 }
-}
-
